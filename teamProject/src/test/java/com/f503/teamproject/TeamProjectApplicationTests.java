2937dca740bd5f01c3cf935548b6e8d6a554c54a
DESKTOP-GJFL5LP\it <ggiu0455@naver.com>
2025-02-11 13:59:57 +0900

    김정

---
+++ teamProject/src/test/java/com/f503/teamproject/TeamProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.f503.teamproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
